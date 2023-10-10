2903a41d63472b8140a244aea91ea55edb0214e6
akkinasairamsrikar-open <akkina.sairamsrikar@bankopen.co>
2023-10-10 16:13:11 +0530

    Pub/Sub EDA

--- message.go
+++ message.go
@@ -1,14 +1,17 @@
 package pubsub
 
+// Message is the struct that is passed to the subscribers
 type Message struct {
 	Topic   string
 	Payload interface{}
 }
 
+// GetTopic returns the topic of the message
 func (m *Message) GetTopic() string {
 	return m.Topic
 }
 
+// GetPayload returns the payload of the message
 func (m *Message) GetPayload() interface{} {
 	return m.Payload
 }
